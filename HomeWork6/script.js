fdb9e70e1b2ddfa97525050cc9eda07e7e7aabb7
alexandermarandici <112718882+alexandermarandici@users.noreply.github.com>
2022-11-20 19:43:18 +0100

    HomeWork6

---
+++ HomeWork6/script.js
@@ -0,0 +1,68 @@
+//task1
+
+let d = 7;
+
+console.log(d, d**2, d**3);
+
+//task2
+//let a;
+//a = parseFloat(window.prompt("What is your Number"));
+//if (a > 0) {
+    //a = a * 2;
+//}
+//console.log(a);
+
+//task3
+//let b = prompt("What is your number")
+//if (b > 0) {
+  //  console.log("positiv");
+//}
+//if (b < 0 ) {
+  //  console.log("negative")
+//}
+//if (b == 0) {
+  //  console.log("Null")
+
+//}
+
+//let a = prompt("Enter first Number")
+//let b = prompt("Enter second Number")
+
+//if (a > b) {
+//console.log(a + b);
+//}
+//if (b < a) {
+//    console.log(a * b);
+//}
+//if (a == b) {
+//    console.log("the numbers are the same")
+//}
+
+//task5
+
+//let a = prompt("Enter your number")
+
+//if (a < 0) {
+    //console.log(a**2);
+//}else{
+    //if (a > 0)
+    //console.log("Error");
+//}
+//task7
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//let mum = prompt("Start")
+
+//for(let i = 0; i < 9; i++);
+//console.log("Start");
+//console.log(arr);
+//console.log("Finish");
+
+
+
+
+
+
+
+
+
+
